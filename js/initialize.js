a82a707c34d99b1d6ff2b4a9a95483f4e3e31df0
Björn Berggren <berggren.bjorn@hotmail.com>
2019-05-22 02:42:59 +0200

    Cleaned up a bit, removed old unused files

--- js/initialize.js
+++
@@ -1,5 +0,0 @@
-let currentLanguage = localStorage.getItem("current_language") || null;
-
-localStorage.setItem('sv_lang', JSON.stringify(sv_lang));
-localStorage.setItem('en_lang', JSON.stringify(en_lang));
-//localStorage.setItem('current_language', JSON.stringify(currentLanguage));
